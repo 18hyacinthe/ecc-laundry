5918cf78af20c654b9342616030b92bcc75b68ca
18Hyacinthe <agbehyavargas@gmail.com>
2024-10-21 12:31:31 +0100

    installation du projet et implementation de la page Auth

---
+++ resources/views/frontend/auth/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+        <meta charset="UTF-8">
+        <title>Double Slider Sign in/up Form</title>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css'><link rel="stylesheet" href="{{ asset('frontend/login/style.css') }}">
+
+    </head>
+    <body>
+
+        
+        <!-- partial:index.partial.html -->
+        @include('frontend.auth.layouts.main')
+        <!-- partial -->
+
+
+    <script  src="{{ asset('frontend/login/script.js') }}"></script>
+
+    </body>
+</html>
