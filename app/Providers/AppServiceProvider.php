5c0c014e281838ddca04873fb84e58d58f231032
18Hyacinthe <agbehyavargas@gmail.com>
2024-10-29 23:59:49 +0100

    deploiement to railway.app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 // use Illuminate\Support\Facades\URL;
-use Illuminate\Routing\UrlGenerator;
+// use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,17 +19,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    // public function boot(): void
-    // {
-    //     // if(config('app.env') === 'local') {
-    //     //     URL::forceScheme('https');
-    //     // }
-    // }
-
-    public function boot(UrlGenerator $url)
+    public function boot(): void
     {
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-        }
+        // if(config('app.env') === 'local') {
+        //     URL::forceScheme('https');
+        // }
     }
+
+    // public function boot(UrlGenerator $url)
+    // {
+    //     if (env('APP_ENV') == 'production') {
+    //         $url->forceScheme('https');
+    //     }
+    // }
 }
