49a4445ebb0048a17b42c8fa4cf1388f361e5844
18Hyacinthe <agbehyavargas@gmail.com>
2024-10-30 20:49:58 +0100

    redeploy to digital ocean by fixing http request inside AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-// use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\URL;
 // use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,9 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if(config('app.env') === 'local') {
-        //     URL::forceScheme('https');
-        // }
+        if(config('app.env') === 'local') {
+            URL::forceScheme('https');
+        }
     }
 
     // public function boot(UrlGenerator $url)
