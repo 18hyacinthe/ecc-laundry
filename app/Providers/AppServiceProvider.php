a3bcf95acc796592a8feec5ed37f7cd0f4cbc742
18Hyacinthe <agbehyavargas@gmail.com>
2024-11-02 00:42:26 +0100

    finition de la verify email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,16 +36,16 @@ class AppServiceProvider extends ServiceProvider
         //     SendWelcomeEmail::class
         // );
 
-        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-            return (new MailMessage)
-                ->subject('Verify Email Address')
-                ->line('Click the button below to verify your email address.')
-                ->action('Verify Email Address', $url);
-        });
-
         // VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-        //     return new VerifyEmailUser($notifiable->email, $url);
+        //     return (new MailMessage)
+        //         ->subject('Verify Email Address')
+        //         ->line('Click the button below to verify your email address.')
+        //         ->action('Verify Email Address', $url);
         // });
+
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return new VerifyEmailUser($notifiable->email, $url);
+        });
     }
 
 }
