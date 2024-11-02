d0e65a177ba27513e753b11b4e0ce7edd2b4f699
18Hyacinthe <agbehyavargas@gmail.com>
2024-11-02 14:41:09 +0100

    expiration du token verify mail

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -1,41 +1,21 @@
 <?php
 
-namespace Illuminate\Auth\Middleware;
+namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\URL;
 
 class EnsureEmailIsVerified
 {
     /**
-     * Specify the redirect route for the middleware.
-     *
-     * @param  string  $route
-     * @return string
-     */
-    public static function redirectTo($route)
-    {
-        return static::class.':'.$route;
-    }
-
-    /**
      * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $redirectToRoute
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|null
      */
-    public function handle($request, Closure $next, $redirectToRoute = null)
+    public function handle(Request $request, Closure $next)
     {
-        if (! $request->user() ||
-            ($request->user() instanceof MustVerifyEmail &&
-            ! $request->user()->hasVerifiedEmail())) {
-            return $request->expectsJson()
-                    ? abort(403, 'Your email address is not verified.')
-                    : Redirect::guest(URL::route($redirectToRoute ?: 'verification.notice'));
+        if (Auth::check() && Auth::user() instanceof MustVerifyEmail && !Auth::user()->hasVerifiedEmail()) {
+            return redirect()->route('verification.notice');
         }
 
         return $next($request);
