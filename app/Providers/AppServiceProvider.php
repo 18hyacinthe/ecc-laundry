c943ed76914f969848010641a7ee443a4b196b97
18Hyacinthe <agbehyavargas@gmail.com>
2024-11-07 21:53:53 +0100

    Calendar admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,9 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'local') {
-            URL::forceScheme('https');
-        }
+        // if (config('app.env') === 'local') {
+        //     URL::forceScheme('https');
+        // }
 
         // Event::listen(
         //     Registered::class,
