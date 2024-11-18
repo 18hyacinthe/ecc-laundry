b0f02db6b671edc7b9f4085c967d09b7a8730445
18Hyacinthe <agbehyavargas@gmail.com>
2024-11-18 20:05:19 +0100

    etat v1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,6 @@ use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Console\Scheduling\Schedule;
-// use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,17 +30,6 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
-        // Event::listen(
-        //     Registered::class,
-        //     SendWelcomeEmail::class
-        // );
-
-        // VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-        //     return (new MailMessage)
-        //         ->subject('Verify Email Address')
-        //         ->line('Click the button below to verify your email address.')
-        //         ->action('Verify Email Address', $url);
-        // });
 
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
             return new VerifyEmailUser($notifiable->email, $url);
