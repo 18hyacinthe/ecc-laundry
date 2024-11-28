9ef206d8fbbb1648db6b6ee6b6b1c416acce8743
18Hyacinthe <agbehyavargas@gmail.com>
2024-11-28 05:52:59 +0100

    Redirection of user

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        if (Auth::check()) {
+            // Redirige en fonction du rôle de l'utilisateur connecté
+            if (Auth::user()->role === 'admin') {
+                return redirect('/admin/dashboard');
+            } elseif (Auth::user()->role === 'user') {
+                return redirect('/user/dashboard');
+            }
+        }
+
+        // Si l'utilisateur n'est pas connecté, affiche la page de connexion
         return view('frontend.auth.master');
-        // return view('auth.login');
     }
 }
